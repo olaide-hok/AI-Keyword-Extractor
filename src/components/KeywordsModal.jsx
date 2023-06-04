d7bd3099b4e06fef4e1c806dae2ed85bed3c47aa
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-06-04 19:03:47 +0100

    Create a KeywordsModal popup component
    Renders a modal to the UI containing the keywords extracted

---
+++ src/components/KeywordsModal.jsx
@@ -0,0 +1,48 @@
+/* eslint-disable react/prop-types */
+import {
+    Button,
+    CircularProgress,
+    Modal,
+    ModalBody,
+    ModalCloseButton,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+    ModalOverlay,
+    Text,
+} from '@chakra-ui/react'
+
+const KeywordsModal = ({keywords, loading, isOpen, closeModal}) => {
+    return (
+        <>
+            <Modal isOpen={isOpen} onClose={closeModal}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalCloseButton />
+                    <ModalHeader>Keywords</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody
+                        display="flex"
+                        alignItems="center"
+                        justifyContent="center">
+                        {loading ? (
+                            <CircularProgress
+                                isIndeterminate
+                                color="blue.300"
+                            />
+                        ) : (
+                            <Text>{keywords}</Text>
+                        )}
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button colorScheme="blue" mr={3} onClick={closeModal}>
+                            Close
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </>
+    )
+}
+
+export default KeywordsModal
